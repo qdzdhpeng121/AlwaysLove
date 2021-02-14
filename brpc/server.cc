b48a0afac20cbd804d032d3553198b7223484083
root <root@localhost.localdomain>
2021-02-14 13:32:46 +0800

    old modified

--- brpc/server.cc
+++ brpc/server.cc
@@ -56,7 +56,7 @@ public:
 
 int main(int argc, char* argv[]) {
     // Parse gflags. We recommend you to use gflags as well.
-    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    GFLAGS_NS::ParseCommandLineFlags(&argc, &argv, true);
 
     // Generally you only need one Server.
     brpc::Server server;
